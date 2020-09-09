b3fbe105dcc840341c10ca262ff666cb3f8d9fb5
manish verma <manishverma@grampower.com>
2020-09-10 01:00:22 +0530

    adding project

---
+++ build/precache-manifest.7db5b029d7f5514299698227ee799dae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970dfe9d53904beb204b5a6bc01686f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b604dba0dd5d2a66967a",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ee804ffabce263ac74f2",
+    "url": "/static/css/main.23146c6b.chunk.css"
+  },
+  {
+    "revision": "b604dba0dd5d2a66967a",
+    "url": "/static/js/2.632b4d1b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.632b4d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee804ffabce263ac74f2",
+    "url": "/static/js/main.42d64699.chunk.js"
+  },
+  {
+    "revision": "c2159e35e941e5b77ed4",
+    "url": "/static/js/runtime-main.358d1757.js"
+  }
+]);
